3ccb3eeb0a9117ebea08378600d755ce61a15127
Avish Kaushik <avish.kaushik20@gmail.com>
2022-01-18 20:03:21 +0530

    Splash Screen Addition

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,5 +1,5 @@
 const settings = {
-  isSplash: false,
+  isSplash: true,
 };
 
 const seo = {
@@ -362,8 +362,7 @@ const degrees = {
       duration: "2018 - Present",
       descriptions: [
         "⚡ I have studied basic software engineering subjects like DS, Algorithms, DBMS, OS, CA, AI etc.",
-        "⚡ Apart from this, I have done courses on Deep Learning, Data Science, Cloud Computing and Full Stack Development.",
-        "⚡ I was selected for Merit cum Means Scholarship which is given to top 10% of students in college. I have received award from respected director for consistently best performance in academics.",
+        "⚡ Apart from this, I have done courses on Web Development, Data Structure and Algorithms, Cloud Computing and Full Stack Development.",
       ],
       website_link: "https://vitbhopal.ac.in/",
     },
